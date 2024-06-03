6c1a053461f0a38a186645cacef811b70e253d82
mmihye <smilehye910@naver.com>
2024-06-03 16:10:15 +0900

    [#24] feat: 마이페이지 기본정보 api

---
+++ src/main/java/Journey/Together/domain/member/dto/MyPageRes.java
@@ -0,0 +1,8 @@
+package Journey.Together.domain.member.dto;
+
+public record MyPageRes(
+        String name,
+        Integer reviewNum,
+        Long date
+) {
+}
--- src/main/java/Journey/Together/domain/member/service/MemberService.java
+++ src/main/java/Journey/Together/domain/member/service/MemberService.java
@@ -1,9 +1,14 @@
 package Journey.Together.domain.member.service;
 
+import Journey.Together.domain.member.dto.MyPageRes;
+import Journey.Together.domain.member.entity.Member;
 import Journey.Together.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,5 +17,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public MyPageRes getMypage(Member member){
+        Long date = Duration.between(member.getCreatedAt(), LocalDateTime.now()).toDays();
+        return new MyPageRes(member.getName(), 0, date);
+    }
 
 }
