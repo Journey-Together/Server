384a3f3dc04b57df384545201026501b349bd088
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-06-07 03:32:21 +0900

    #45 guest일 경우 api 구현

--- src/main/java/Journey/Together/global/config/SecurityConfig.java
+++ src/main/java/Journey/Together/global/config/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig {
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/v1/member/**").authenticated()
                         .requestMatchers("/v1/place/main").permitAll()
+                        .requestMatchers("/v1/plan/guest/**").permitAll()
                         // 메인 페이지, 공고 페이지 등에 한해 인증 정보 없이 접근 가능 (추후 추가)
                         // 이외의 모든 요청은 인증 정보 필요
                         .anyRequest().authenticated());
