f36430526bec4fbfeb4fe3dd73038d8bd156f859
mmihye <smilehye910@naver.com>
2024-07-26 00:36:35 +0900

    fix: 지도 검색 security 권한설정

--- src/main/java/Journey/Together/global/config/SecurityConfig.java
+++ src/main/java/Journey/Together/global/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
                         .requestMatchers("/v1/plan/open").permitAll()
                         .requestMatchers("/v1/plan/search").permitAll()
                         .requestMatchers("/v1/place/search").permitAll()
+                        .requestMatchers("/v1/place/search/**").permitAll()
                         .requestMatchers("/v1/place/guest/**").permitAll()
                         // 메인 페이지, 공고 페이지 등에 한해 인증 정보 없이 접근 가능 (추후 추가)
                         // 이외의 모든 요청은 인증 정보 필요
