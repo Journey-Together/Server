0b6252dc7ffcd72116bfaa7da82aeaabbd5a3863
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-08-23 20:33:39 +0900

    #137 feat:  MatchingProperties 빈 등록 선언

---
+++ src/main/java/Journey/Together/domain/place/service/match/MatchingConfig.java
@@ -0,0 +1,9 @@
+package Journey.Together.domain.place.service.match;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MatchingProperties.class)
+public class MatchingConfig {
+}
