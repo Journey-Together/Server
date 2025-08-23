9ebe61eb9fd09b1dd8c3070a41163c3fd9a5bd02
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-08-23 20:30:31 +0900

    #137 feat: PlacMatchIssue repository 생성

---
+++ src/main/java/Journey/Together/domain/place/repository/PlaceMatchIssueRepository.java
@@ -0,0 +1,8 @@
+package Journey.Together.domain.place.repository;
+
+import Journey.Together.domain.place.entity.PlaceMatchIssue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceMatchIssueRepository extends JpaRepository<PlaceMatchIssue, Long> {
+
+}
